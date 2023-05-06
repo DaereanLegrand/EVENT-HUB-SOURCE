a0e2eb8f520390eb1c5cff5fc8f47eda6d315efc
Daerean Legrand <frankrogerstg@gmail.com>
2023-05-06 00:35:42 -0500

    Full register cycle

--- public/register.js
+++ public/register.js
@@ -4,7 +4,8 @@ window.addEventListener("DOMContentLoaded", function() {
 })
 
 
-function samepass() {
+function samepass(event) {
+  event.preventDefault();
   var pass1 = document.getElementById("upass").value;
   var pass2 = document.getElementById("upassconf").value;
 
@@ -27,11 +28,12 @@ function samepass() {
       }) 
       .then(response => response.json())
         .then(data => {
-            console.log("AAAA");
-            document.getElementById("show-error").style("color: green;");
-            document.getElementById("show-error").innerText = "Usted se registro correctamente.";
+            if (data[0].success == true) {
+                document.getElementById("show-error").style("color: green;");
+                document.getElementById("show-error").innerText = "Usted se registro correctamente.";
+            }
         })
-      return false;
+      document.getElementById("form-register").submit();
   }
 
 }
